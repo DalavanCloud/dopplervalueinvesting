a2d235b9b1dd5aaa2e4e6c5bee8ef726436a206a
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-11 16:26:05 -0600

    Save screen output in nightly.sh

--- nightly.sh
+++ nightly.sh
@@ -2,5 +2,6 @@
 
 # This script is strictly for the web server environment.
 # The nice command makes the script a sufficiently low priority to avoid impacting performance.
+# The screen output is logged and saved.
 
-nice -n10 ionice -c2 -n5 python2.7 $HOME/webapps/scripts_doppler/dopplervalueinvesting/screen.py
+nice -n10 ionice -c2 -n5 python2.7 $HOME/webapps/scripts_doppler/dopplervalueinvesting/screen.py &>> $HOME/logs/user/screen.txt
