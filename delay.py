b4fb311fa67b768703ad8b824f45265f728f271e
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-10 20:28:29 -0600

    Corrected delay.py

--- delay.py
+++ delay.py
@@ -6,9 +6,10 @@
 import time, random
 
 def delay_min (n_minutes_max):
-    n_sec_max = n_minutes * 60
-    n_sec = random.uniform (0, n_sec)
-    print "Delay (minutes): " + str(n_sec/60)
+    n_sec_max = n_minutes_max * 60
+    n_sec = random.uniform (0, n_sec_max)
+    n_minutes = n_sec/60
+    print "Delay (minutes): " + str(n_minutes)
     time.sleep (random.uniform (0, n_sec))
 
 delay_min (0)
