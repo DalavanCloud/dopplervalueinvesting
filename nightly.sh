3e29819bd6054f60e5972863d90e5df449ff0089
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-11 00:16:06 -0600

    Removed delay.py and nightly.sh

--- nightly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Proper header for a Bash script.
-
-# This is the script called by cron on the remote server.  
-# This script is NOT intended for use in the development environment.
-
-python $HOME/webapps/scripts_doppler/dopplervalueinvesting/delay.py
-python $HOME/webapps/scripts_doppler/dopplervalueinvesting/screen.py
