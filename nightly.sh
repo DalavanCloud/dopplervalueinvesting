f35c7e4d3efbb2abb8fb54aba3bc4b7e47d0d572
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-11 16:21:56 -0600

    Added nightly.sh for server setup

---
+++ nightly.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is strictly for the web server environment.
+# The nice command makes the script a sufficiently low priority to avoid impacting performance.
+
+nice -n10 ionice -c2 -n5 python2.7 $HOME/webapps/scripts_doppler/dopplervalueinvesting/screen.py
