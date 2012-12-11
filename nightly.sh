615f2737ffa1c54916dadf6e00fb430b872f4d94
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-11 16:39:14 -0600

    Disabled nice in nightly.sh

--- nightly.sh
+++ nightly.sh
@@ -4,4 +4,6 @@
 # The nice command makes the script a sufficiently low priority to avoid impacting performance.
 # The screen output is logged and saved.
 
-nice -n10 ionice -c2 -n5 python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/screen.py &>> $HOME/logs/user/screen.txt
+#nice -n10 ionice -c2 -n5 python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/screen.py &>> $HOME/logs/user/screen.txt
+
+python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/screen.py &>> $HOME/logs/user/screen.txt
