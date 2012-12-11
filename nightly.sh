57063a40dc5b3d6d9f16a3386418f8f33f6e3256
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-11 17:30:44 -0600

    Added notes on cron setup

--- nightly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is strictly for the cron execution in the web server environment.
-# The nice command makes the script a sufficiently low priority to avoid impacting performance.
-# The screen output is logged and saved.
-
-#nice -n10 ionice -c2 -n5 python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/screen.py &>> $HOME/logs/user/screen.txt
-
-python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/screen.py &>> $HOME/logs/user/screen.txt
