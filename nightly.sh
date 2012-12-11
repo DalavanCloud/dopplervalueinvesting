007b8bf26995d2cadc5ec50b2b3dfd2b8f31b543
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-11 16:31:12 -0600

    Explicitly spelled out path

--- nightly.sh
+++ nightly.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This script is strictly for the web server environment.
+# This script is strictly for the cron execution in the web server environment.
 # The nice command makes the script a sufficiently low priority to avoid impacting performance.
 # The screen output is logged and saved.
 
-nice -n10 ionice -c2 -n5 python2.7 $HOME/webapps/scripts_doppler/dopplervalueinvesting/screen.py &>> $HOME/logs/user/screen.txt
+nice -n10 ionice -c2 -n5 python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/screen.py &>> $HOME/logs/user/screen.txt
