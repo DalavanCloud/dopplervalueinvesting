14d7d22300c79b98310eaa30ca66ed73f8052ea4
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-10 19:36:22 -0600

    Changed paths to log files

--- nightly.sh
+++ nightly.sh
@@ -3,5 +3,5 @@
 
 # This is the script called by cron on the remote server.
 
-nice -n10 ionice -c2 -n5 python $HOME/webapps/scripts_doppler/dopplervalueinvesting/delay.py | tee $HOME/logs/delay.txt
-nice -n10 ionice -c2 -n5 python $HOME/webapps/scripts_doppler/dopplervalueinvesting/screen.py | tee $HOME/logs/screen.txt
+nice -n10 ionice -c2 -n5 python $HOME/webapps/scripts_doppler/dopplervalueinvesting/delay.py | tee $HOME/logs/user/delay.txt
+nice -n10 ionice -c2 -n5 python $HOME/webapps/scripts_doppler/dopplervalueinvesting/screen.py | tee $HOME/logs/user/screen.txt
