06022ef29cb0fbd1633c33d770b62f7301a23c4f
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-15 14:06:17 -0600

    Moved delay function to delay.py, added nightly.sh function to call delay.py and screen.py

---
+++ delay.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+# This script provides a random delay and is intended for use ONLY on the server.
+
+import random, time
+
+def delay_min (n_minutes_max):
+    n_sec_max = n_minutes_max * 60
+    n_sec = random.uniform (0, n_sec_max)
+    n_minutes = n_sec/60
+    print "Delay (minutes): " + str(n_minutes)
+    time.sleep (random.uniform (0, n_sec))
+
+delay_min (0)
