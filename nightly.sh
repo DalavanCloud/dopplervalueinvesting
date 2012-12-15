c20bcebe3a48f43216c1c38f20acdb6107cb47d8
Jason Hsu <jhsu-android@jasonhsu.com>
2012-12-15 14:09:27 -0600

    Corrected nightly.sh

--- nightly.sh
+++ nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Proper header for a Bash script.
 
-nice -n10 ionice -c2 -n5 /usr/local/bin/python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/python.py >> /home/doppler/logs/user/delay.txt
+nice -n10 ionice -c2 -n5 /usr/local/bin/python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/delay.py >> /home/doppler/logs/user/delay.txt
 
 nice -n10 ionice -c2 -n5 /usr/local/bin/python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/screen.py >> /home/doppler/logs/user/screen.txt
