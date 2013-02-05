6b8eefcbbba96ffc512c6ae865a3541c8f69899d
Jason Hsu <jhsu802701@jasonhsu.com>
2013-02-05 00:03:21 -0600

    Updated nightly.sh

--- nightly.sh
+++ nightly.sh
@@ -4,3 +4,5 @@
 nice -n10 ionice -c2 -n5 /usr/local/bin/python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/delay.py
 
 nice -n10 ionice -c2 -n5 /usr/local/bin/python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/screen.py
+
+nice -n10 ionice -c2 -n5 /usr/local/bin/python2.7 /home/doppler/webapps/scripts_doppler/dopplervalueinvesting/stock.py
